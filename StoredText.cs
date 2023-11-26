482ec5ed0656c4a29a6125e5b0e2725b6c923fcd
Dillon Carter <73437822+PickleMustard@users.noreply.github.com>
2023-11-26 13:17:20 -0600

    Add files via upload

---
+++ StoredText.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoredText : MonoBehaviour {
+    public string text;
+    public int num;
+    public bool final_clue;
+}
