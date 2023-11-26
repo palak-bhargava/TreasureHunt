f7283227481f87dbf7bb54cf51f078e8a78e42f8
Dillon Carter <73437822+PickleMustard@users.noreply.github.com>
2023-11-26 16:25:09 -0600

    Delete StoredText.cs

--- StoredText.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class StoredText : MonoBehaviour {
-    public string text;
-    public int num;
-    public bool final_clue;
-}
