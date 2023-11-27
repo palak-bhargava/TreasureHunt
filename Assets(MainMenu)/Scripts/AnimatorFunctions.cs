cfb665093a80eea0bc23683c74dd1cd43c4a3287
Emin Sarac <124945009+77EminSarac77@users.noreply.github.com>
2023-11-27 11:10:16 -0600

    MainMenu start up of game.

---
+++ Assets(MainMenu)/Scripts/AnimatorFunctions.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorFunctions : MonoBehaviour
+{
+    [SerializeField] MenuButtonController menuButtonController;
+    public bool disableOnce;
+    
+    public void PlaySound(AudioClip whichSound)
+    {
+        if(!disableOnce)
+        {
+            menuButtonController.audioSource.PlayOneShot (whichSound);
+        }
+        else
+        {
+            disableOnce = false;
+        }
+    }
+}
